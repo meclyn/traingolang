41772529f08bbf4bd266ddaa1092e1b104f0a31d
Matheus <matheusferreirasjc@gmail.com>
2024-06-14 01:05:28 -0300

    my first script in go!

---
+++ main.go
@@ -0,0 +1,36 @@
+package main 
+
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+func main (){
+	// cria novo leitor para ler o input do user
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Digite um número: ")
+
+	// lê o input do user ate um caractere
+	input, _ := reader.ReadString('\n')
+
+	// Remove espaço em branco no input
+	input = strings.TrimSpace(input)
+
+	//converte a string do input para int
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.PrintIn("Erro: por favor, digite um número válido.")
+		return
+	}
+
+	// verifica se o numero é par ou impar
+	if number%2 == 0 {
+		fmt.Printf("O número %d é par.\n", number)
+	} else {
+		fmt.Printf("O número %d é ímpar.\n", number)
+	}
+}
